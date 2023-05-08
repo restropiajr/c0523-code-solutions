beb7bdcd961bcfd4f822cd5f97c24ace25d41aa1
Roland Estropia Jr <restropiajr@gmail.com>
2023-05-08 20:39:02 +0000

    javascript-primitives-and-variables - Roland Estropia Jr.

---
+++ javascript-primitives-and-variables/javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+const fullName = 'Roland Estropia Jr.';
+const isCool = true;
+const totalPets = 2;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeOf fullName:', typeof fullName);
+console.log('typeOf isCool:', typeof isCool);
+console.log('typeOf totalPets:', typeof totalPets);
+
+const unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+let never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
