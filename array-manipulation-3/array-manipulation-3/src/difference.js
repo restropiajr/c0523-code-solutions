5129b18240bde8807ba490c3586359d57cea8b4a
Roland Estropia Jr <restropiajr@gmail.com>
2023-05-19 17:15:44 +0000

    Updated difference.js and flatten.js

--- array-manipulation-3/array-manipulation-3/src/difference.js
+++ array-manipulation-3/array-manipulation-3/src/difference.js
@@ -3,7 +3,6 @@ function difference(first, second) {
   const result = [];
 
   const combined = [...first, ...second];
-  console.log(combined);
 
   for (const el of combined) {
     if (first.includes(el) && second.includes(el)) {
--- array-manipulation-3/array-manipulation-3/src/flatten.js
+++ array-manipulation-3/array-manipulation-3/src/flatten.js
@@ -1,10 +1,8 @@
 /* exported flatten */
 function flatten(array) {
   const result = [];
-  console.log(array);
 
   for (const el of array) {
-    console.log(el);
     if (Array.isArray(el)) {
       for (const val of el) {
         result.push(val);
