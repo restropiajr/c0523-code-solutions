a849d55d437a35f7923d1b4a3603cd89f7efafd2
Roland Estropia Jr <restropiajr@gmail.com>
2023-05-22 23:51:16 +0000

    Updated main.js

--- dom-manipulation/dom-manipulation/main.js
+++ dom-manipulation/dom-manipulation/main.js
@@ -9,15 +9,15 @@ function handleClickCount(event) {
 
   if (count > 0 && count < 4) {
     $hotBtn.className = 'hot-button cold';
-  } else if (count > 4 && count < 7) {
+  } else if (count >= 4 && count < 7) {
     $hotBtn.className = 'hot-button cool';
-  } else if (count > 7 && count < 10) {
+  } else if (count >= 7 && count < 10) {
     $hotBtn.className = 'hot-button tepid';
-  } else if (count > 10 && count < 13) {
+  } else if (count >= 10 && count < 13) {
     $hotBtn.className = 'hot-button warm';
-  } else if (count > 13 && count < 16) {
+  } else if (count >= 13 && count < 16) {
     $hotBtn.className = 'hot-button hot';
-  } else if (count > 16) {
+  } else if (count >= 16) {
     $hotBtn.className = 'hot-button nuclear';
   }
 }
