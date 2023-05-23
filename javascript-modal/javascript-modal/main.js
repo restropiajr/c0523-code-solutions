82c6e81c93c077c4fd72c9841ac878eca7be048e
Roland Estropia Jr <restropiajr@gmail.com>
2023-05-23 21:04:49 +0000

    Updated styles.css and main.js

--- javascript-modal/javascript-modal/main.js
+++ javascript-modal/javascript-modal/main.js
@@ -4,8 +4,10 @@ const $modalContainer = document.querySelector('.modal-container.hidden');
 
 $modalButton.addEventListener('click', () => {
   $modalContainer.classList.remove('hidden');
+  console.log($modalContainer.classList);
 });
 
 $noButton.addEventListener('click', () => {
   $modalContainer.classList.add('hidden');
+  console.log($modalContainer.classList);
 });
