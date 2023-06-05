9e418199322aebedb65665b977ad6f28bde6f75c
Roland Estropia Jr <restropiajr@gmail.com>
2023-06-05 20:58:35 +0000

    Updated interval.js

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,9 +1,10 @@
-function countdown() {
-  const $countdownDisplay = document.querySelector('.countdown-display');
-  const time = Number($countdownDisplay.textContent);
+const $countdownDisplay = document.querySelector('.countdown-display');
+let time = Number($countdownDisplay.textContent);
 
+function countdown() {
   if (time > 1) {
     $countdownDisplay.textContent = time - 1;
+    time -= 1;
   } else {
     clearInterval(countDown);
     $countdownDisplay.textContent = '~Earth Beeeelooowww Us~';
