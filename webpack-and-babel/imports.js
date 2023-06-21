c97fecf71eeeb4c86e3e2690426ae69ab713b292
Roland Estropia Jr <restropiajr@gmail.com>
2023-06-21 18:04:08 +0000

    Removed file type from the path

--- webpack-and-babel/imports.js
+++ webpack-and-babel/imports.js
@@ -2,21 +2,21 @@
 
 // import from a file located at the absolute path `/foo/bar/baz/qux.js`:
 // eslint-disable-next-line import/no-absolute-path
-import qux from '/foo/bar/baz/qux.js';
+import qux from '/foo/bar/baz/qux';
 
 // import from a file named `foo.json` located in the current directory:
-import foo from './foo.json';
+import foo from './foo';
 
 // import from a file named `bar.js` located in the parent directory:
-import bar from '../bar.js';
+import bar from '../bar';
 
 // import from a file named `index.js` located in a directory named `baz` that
 // is in the current directory:
-import index from './baz/index.js';
+import index from './baz';
 
 // import from a file named `index2.js` located in a directory named `baz` that
 // is in the parent directory:
-import index2 from '../baz/index2.js';
+import index2 from '../baz';
 
 // import from an `npm` module named `react`:
 import React from 'react';
