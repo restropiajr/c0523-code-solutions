5df4d3e9cc8745441526c12f80e06d0aeeebc27f
Roland Estropia Jr <restropiajr@gmail.com>
2023-06-26 17:45:36 +0000

    Removed coefficients from O(n) and O(n^2)

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -5,14 +5,14 @@ function uniqueLinear(words) {
   const unique = []; // 1 * 1 = O(1)
   for (
     let i = 0; // 1 * 1 = O(1)
-    i < words.length; // 2 * n = O(2n)
-    i++ // 2 * n = O(2n)
+    i < words.length; // 2 * n = O(n)
+    i++ // 2 * n = O(n)
   ) {
-    const word = words[i]; // 2 * n = O(2n)
+    const word = words[i]; // 2 * n = O(n)
     if (!seen[word]) {
-      // 2 * n = O(2n)
-      seen[word] = true; // 2 * n = O(3n)
-      unique[unique.length] = word; // 3 * n = O(3n)
+      // 2 * n = O(n)
+      seen[word] = true; // 2 * n = O(n)
+      unique[unique.length] = word; // 3 * n = O(n)
     }
   }
   return unique; // 1 * 1 = O(1)
@@ -22,25 +22,25 @@ function uniqueQuadratic(words) {
   const unique = []; // 1 * 1 = O(1)
   for (
     let i = 0; // 1 * 1 = O(1)
-    i < words.length; // 2 * n = O(2n)
-    i++ // 2 * n = O(2n)
+    i < words.length; // 2 * n = O(n)
+    i++ // 2 * n = O(n)
   ) {
-    const word = words[i]; // 2 * n = O(2n)
-    let isUnique = true; // 2 * n = O(2n)
+    const word = words[i]; // 2 * n = O(n)
+    let isUnique = true; // 2 * n = O(n)
     for (
-      let c = 0; // 1 * n = O(1n)
+      let c = 0; // 1 * n = O(n)
       c < i; // 1 * n * n = O(n^2)
-      c++ // 2 * n * n = O(2n^2)
+      c++ // 2 * n * n = O(n^2)
     ) {
-      const comparing = words[c]; // 2 * n * n = O(2n^2)
+      const comparing = words[c]; // 2 * n * n = O(n^2)
       if (comparing === word) {
-        // 1 * n * n = O(1n^2)
-        isUnique = false; // 1 * n * n = O(1n^2)
+        // 1 * n * n = O(n^2)
+        isUnique = false; // 1 * n * n = O(n^2)
       }
     }
     if (isUnique) {
-      // 1 * n = O(1n)
-      unique[unique.length] = word; // 3 * n = O(3n))
+      // 1 * n = O(n)
+      unique[unique.length] = word; // 3 * n = O(n))
     }
   }
   return unique; // 1 * _1 = O(1)
