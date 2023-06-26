a2b7338f8e12e1d330d14c370d281ec7b3c529bf
Roland Estropia Jr <restropiajr@gmail.com>
2023-06-26 19:09:34 +0000

    Started react app

---
+++ javascript-light-bulb/my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LightBulb from './LightBulb';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <LightBulb />
+    </div>
+  );
+}
+
+export default App;
