352b15eec29467c2ac746b90958ddd8b1d7141c4
Roland Estropia Jr <restropiajr@gmail.com>
2023-07-06 23:53:58 +0000

    Addressed Robert's suggestions

--- node-module-system/calculate.js
+++ node-module-system/calculate.js
@@ -3,32 +3,23 @@ import multiply from './multiply.js';
 import subtract from './subtract.js';
 import divide from './divide.js';
 
+const x = Number(process.argv[2]);
+const y = Number(process.argv[4]);
+
 switch (process.argv[3]) {
   case 'plus':
-    console.log(
-      'Result:',
-      add(Number(process.argv[2]), Number(process.argv[4]))
-    );
+    console.log('Result:', add(x, y));
     break;
   case 'times':
-    console.log(
-      'Result:',
-      multiply(Number(process.argv[2]), Number(process.argv[4]))
-    );
+    console.log('Result:', multiply(x, y));
     break;
   case 'minus':
-    console.log(
-      'Result:',
-      subtract(Number(process.argv[2]), Number(process.argv[4]))
-    );
+    console.log('Result:', subtract(x, y));
     break;
   case 'over':
-    console.log(
-      'Result:',
-      divide(Number(process.argv[2]), Number(process.argv[4]))
-    );
+    console.log('Result:', divide(x, y));
     break;
   default:
-    console.log('Invalid Operation');
+    console.log(`Invalid Operation - ${process.argv[3]}`);
     break;
 }
