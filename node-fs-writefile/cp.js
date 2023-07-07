0a6cf2d125c5786355945fa2f856013b007b5dcd
Roland Estropia Jr <restropiajr@gmail.com>
2023-07-07 18:20:04 +0000

    Removed utf8

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -1,7 +1,7 @@
 import { writeFile, readFile } from 'node:fs/promises';
 
 async function cp() {
-  const contents = await readFile(process.argv[2], 'utf8');
+  const contents = await readFile(process.argv[2]);
 
   const copyFile = String(process.argv[3]);
   await writeFile(copyFile, `${contents}\n`);
