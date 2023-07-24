28aef4e7f7c7efa7a8f2f2877aff7a426b64750e
Roland Estropia Jr <restropiajr@gmail.com>
2023-07-24 18:24:36 +0000

    unit-tests-jest - Roland Estropia Jr.

---
+++ unit-tests-jest/ts/numbers.ts
@@ -0,0 +1,47 @@
+/**
+ * Returns a new array that contains only the even numbers in the given array.
+ */
+export function evenNumbers(numbers: number[]): number[] {
+  return numbers.filter((n) => n % 2 === 0);
+}
+
+/**
+ * Returns a number formatted in dollars and cents.
+ */
+export function toDollars(amount: number): string {
+  return `$${amount.toFixed(2)}`;
+}
+
+/**
+ * Returns a new array of numbers where every entry has been divided by the
+ * given divisor. Does not modify the original array.
+ */
+export function divideBy(numbers: number[], divisor: number): number[] {
+  const result: number[] = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 0 && divisor === 0) {
+      result.push(NaN);
+    }
+    numbers[i] = numbers[i] / divisor;
+  }
+  return numbers;
+}
+
+/**
+ * Modifies an object by multiplying the value of each key, but only if that
+ * value is a number. Key names are arbitrary.
+ */
+export function multiplyBy(
+  obj: Record<string, unknown>,
+  multiplier: number
+): Record<string, unknown> {
+  const result: Record<string, unknown> = {};
+  Object.entries(obj).forEach(([key, value]) => {
+    if (typeof value === 'number') {
+      result[key] = Number(value) * multiplier;
+    } else {
+      result[key] = value;
+    }
+  });
+  return result;
+}
